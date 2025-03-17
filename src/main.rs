056001398a5957a4876993245a0b3ca2924c8231
quapka <>
2025-03-17 10:08:02 +0100

    Mimic Sipa's utility a bit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 // use clap::Parser;
-use miniscript::{Miniscript,Segwitv0};
+use miniscript::BareCtx;
+use miniscript::Error;
+use miniscript::Legacy;
+use miniscript::{policy, Miniscript, Segwitv0};
+use policy::Liftable;
 use std::str::FromStr;
 // use std::io::{self};
-
-
 
 // #[derive(Parser, Debug)]
 // #[command(version, about, long_about = None)]
@@ -11,9 +13,11 @@ use std::str::FromStr;
 //     policy: String,
 // }
 
-
 fn main() {
     // let args = Args::parse();
+
+    type Script = Miniscript<String, Legacy>;
+    type Policy = policy::Concrete<String>;
 
     // let stdin = io::stdin();
     for line in std::io::stdin().lines() {
@@ -27,13 +31,31 @@ fn main() {
         //     Ok(line) => line.retain(|c| !c.is_whitespace()),
         //     Err(error) => panic!("Error: {}", error),
         // };
-
-        let ms = match Miniscript::<String, Segwitv0>::from_str(&line) {
-            Ok(ms) => ms,
+        let pol = match Policy::from_str(&line) {
+            Ok(pol) => pol,
             Err(error) => panic!("Error: {}", error),
         };
-    println!("{}", ms.script_size());
-    // Sipa: println!("X {%17.10f} %5i %s %s\n", ret->ScriptSize() + avgcost, (int)ret->ScriptSize(), Abbreviate(std::move(*str)).c_str(), line.c_str());kk
-}
+        if let Ok(desc) = pol.compile::<Legacy>() {
+            // Lift
+            // assert_eq!(desc.lift().unwrap().sorted(), pol.lift().unwrap().sorted());
+            // // Try to roundtrip the output of the compiler
+            // print!("{}", desc.script_size());
+            let output = desc.to_string();
+            if let Ok(desc) = Script::from_str(&output) {
+                let _ = desc.to_string();
+                println!("{} {} {}", desc.script_size(), desc, pol);
+                // assert_eq!(output.to_lowercase(), rtt.to_lowercase());
+            } else {
+                panic!("compiler output something unparseable: {}", output)
+            }
+        }
+        // println!("{}", pol);
 
+        // let ms = match Miniscript::<String, Legacy>::from_str(&line) {
+        //     Ok(ms) => ms,
+        //     Err(error) => panic!("Error: {}", error),
+        // };
+        // println!("{}", ms.script_size());
+        // Sipa: println!("X {%17.10f} %5i %s %s\n", ret->ScriptSize() + avgcost, (int)ret->ScriptSize(), Abbreviate(std::move(*str)).c_str(), line.c_str());kk
+    }
 }
